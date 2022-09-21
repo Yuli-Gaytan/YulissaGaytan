65b01aab6de0892d658b372c8fe0919ea97bf373
Yuli-Gaytan <114116787+Yuli-Gaytan@users.noreply.github.com>
2022-09-21 18:15:12 -0500

    Add files via upload

---
+++ Practica2/Practica2/acciones.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Forms;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    internal class acciones
+    {
+        public static void Mensaje(string mensaje)
+        {
+            MessageBox.Show(mensaje, "AVISO!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public static void LlenarCombo(String consulta, ComboBox combo, string id, string campo)
+        {
+            DataTable dt;
+            dt = conexion.EjecutaSeleccion(consulta);
+
+            DataRow r = dt.NewRow();
+            r[0] = 0;
+            r[1] = "Todos Los Datos";
+            dt.Rows.InsertAt(r, 0);
+
+            if (dt == null)
+            {
+                return;
+            }
+            combo.Items.Clear();
+            combo.DataSource = dt;
+            combo.ValueMember = id;
+            combo.DisplayMember = campo;
+
+        }
+    }
+}
